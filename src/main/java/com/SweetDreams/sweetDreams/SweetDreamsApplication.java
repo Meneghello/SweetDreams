6bcbf2eeb5f57834b6076520b524ffa9dfc3d5c6
Gabriel Meneghello <gabriel.meneghello99@gmail.com>
2022-01-12 16:37:48 -0300

    teste circleCI v2

--- src/main/java/com/SweetDreams/sweetDreams/SweetDreamsApplication.java
+++ src/main/java/com/SweetDreams/sweetDreams/SweetDreamsApplication.java
@@ -1,12 +1,15 @@
 package com.SweetDreams.sweetDreams;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class SweetDreamsApplication {
 
     @GetMapping(value = "")
