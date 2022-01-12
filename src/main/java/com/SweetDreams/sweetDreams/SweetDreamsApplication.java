c4f04e34b4ebe7fce2ee6c66c8bc6acf901e696e
Gabriel Meneghello <gabriel.meneghello99@gmail.com>
2022-01-12 17:33:03 -0300

    teste circleCI v4

--- src/main/java/com/SweetDreams/sweetDreams/SweetDreamsApplication.java
+++ src/main/java/com/SweetDreams/sweetDreams/SweetDreamsApplication.java
@@ -1,9 +1,7 @@
 package com.SweetDreams.sweetDreams;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
