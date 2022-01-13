60ad232fc7242b1b433bb185bf334d8e24b6a6f0
Gabriel Meneghello <gabriel.meneghello99@gmail.com>
2022-01-13 13:35:12 -0300

    authorization e auth

--- src/main/java/com/SweetDreams/sweetDreams/Security/SecurityConfig.java
+++ src/main/java/com/SweetDreams/sweetDreams/Security/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.SweetDreams.sweetDreams.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -16,13 +19,19 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
     //Lista de requisições liberadas
     private static final String[] PUBLIC_REQUESTS = {
-            "/**"
+            //"/**"
     };
     //Lista de requisições liberadas
-    private static final String[] PUBLIC_REQUESTS_ONLY_READ = {
-            //"/produto/**",
+    private static final String[] PUBLIC_REQUESTS_READ_ONLY = {
+            "/produto/**",
 
     };
 
@@ -30,10 +39,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, PUBLIC_REQUESTS_ONLY_READ).permitAll()
+                .antMatchers(HttpMethod.GET, PUBLIC_REQUESTS_READ_ONLY).permitAll()
                 .antMatchers(PUBLIC_REQUESTS).permitAll()
                 .anyRequest().authenticated();
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(),jwtUtil,userDetailsService));
+        http.addFilter(new JWTAuthFilter(authenticationManager(), jwtUtil));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     //Cors - liberação para qualquer requisição
---
+++ src/main/java/com/SweetDreams/sweetDreams/Services/Impl/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.SweetDreams.sweetDreams.Services.Impl;
+
+import com.SweetDreams.sweetDreams.Models.Cliente;
+import com.SweetDreams.sweetDreams.Repository.ClienteRepository;
+import com.SweetDreams.sweetDreams.Security.UserDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    ClienteRepository clienteRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String cpf) throws UsernameNotFoundException {
+        Cliente cliente = clienteRepository.findByCpf(cpf);
+        if (cliente==null){
+            throw new UsernameNotFoundException(cpf);
+        }
+        return new UserDetail(cliente.getId(),cliente.getSenha(),cliente.getCpf(),cliente.getRole());
+    }
+}
